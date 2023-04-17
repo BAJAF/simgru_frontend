a3de40cd80936c640c112f60295c693d262ed444
benjabolanos <bbolanos@uabc.edu.mx>
2023-04-17 01:03:47 -0700

    SIM-21 Pagina de cursos

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,15 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () => import('@/views/Home.vue'),
+      },
+      {
+        path: 'cursos',
+        name: 'Cursos',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/CoursesPage.vue'),
       },
     ],
   },
---
+++ src/views/CoursesPage.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Cursos que administras:</h1>
+
+</template>
