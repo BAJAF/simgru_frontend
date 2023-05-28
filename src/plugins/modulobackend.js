00d8977092110b56544358b0c92829b482dd52d1
AngelLedesma <82411060+J-Angel-Ledesma@users.noreply.github.com>
2023-05-28 01:42:15 -0700

    SIM-64
    SIM-64
    "Guardar el token en el localstorage para acceder a el en las llamadas al backend"

--- src/plugins/modulobackend.js
+++ src/plugins/modulobackend.js
@@ -1,39 +1,42 @@
 import axios from "axios";
 
 const sendTokenToServer = (token, router, jwtStore) => {
-    axios
-        .get("http://localhost:8000/jwt/" + token + "/")
-        .then((res) => {
-            jwtStore.set(res.data.jwt);
-            console.log(res.data.jwt);
-            router.push({
-                name: "Cursos",
-            });
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  axios
+    .get("http://localhost:8000/jwt/" + token + "/")
+    .then((res) => {
+      const jwt = res.data.jwt;
+      localStorage.setItem("token", jwt);
+      //jwtStore.set(jwt);
+      console.log(jwt);
+      router.push({
+        name: "Cursos",
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 const getUserCourses = (cursos, jwtStore) => {
-    let jwt = jwtStore.jwt;
+  const jwt = localStorage.getItem("token");
+  //let jwt = jwtStore.jwt;
 
-    const axInstance = axios.create({
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        },
+  const axInstance = axios.create({
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  axInstance
+    .get("http://localhost:8000/courses/" + jwt)
+    .then((res) => {
+      console.log(res);
+      cursos.value = res.data.courses;
+    })
+    .catch((error) => {
+      console.error(error);
     });
-
-    axInstance
-        .get("http://localhost:8000/courses/" + jwt)
-        .then((res) => {
-            console.log(res);
-            cursos.value = res.data.courses;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
 };
 
 export { sendTokenToServer, getUserCourses };
-
